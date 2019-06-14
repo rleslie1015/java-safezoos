9c8772cbecf3c5d60e2b4b3e071ae7d2fd4f0f89
Leslie <rleslie1015@gmail.com>
2019-06-13 20:54:46 -0500

    added more files

---
+++ zoos/src/main/java/com/lambdaschool/zoos/service/UserService.java
@@ -0,0 +1,18 @@
+package com.lambdaschool.zoos.service;
+
+import com.lambdaschool.zoos.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    User findUserById(long id);
+
+    void delete(long id);
+
+    User save(User user);
+
+    User update(User user, long id);
+}
